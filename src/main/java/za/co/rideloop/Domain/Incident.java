2cb642c3080a4487716d668e378b8e3dc2978e2e
RatiaSB <230724477@mycput.ac.za>
2025-05-12 12:05:41 +0200

    coded domain class with builder class

--- src/main/java/za/co/rideloop/Domain/Incident.java
+++ src/main/java/za/co/rideloop/Domain/Incident.java
@@ -1,4 +1,5 @@
 package za.co.rideloop.Domain;
+import java.util.Objects;
 
 /**
  * RideLoop
@@ -11,5 +12,83 @@ package za.co.rideloop.Domain;
  * @Java version: "21.0.3" 2024-04-16 LTS
  */
 public class Incident {
+    private final int incidentID;
+    private final String incidentType;
+    private final String description;
 
+    /**
+     * Private constructor used by the Builder.
+     * Can only be instantiated through the Builder.
+     */
+    private Incident(Builder builder) {
+        this.incidentID = builder.incidentID;
+        this.incidentType = builder.incidentType;
+        this.description = builder.description;
+    }
+
+    // Getters
+    public int getIncidentID() {
+        return incidentID;
+    }
+
+    public String getIncidentType() {
+        return incidentType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Incident incident = (Incident) o;
+        return incidentID == incident.incidentID &&
+                Objects.equals(incidentType, incident.incidentType) &&
+                Objects.equals(description, incident.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(incidentID, incidentType, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Incident{" +
+                "incidentID=" + incidentID +
+                ", incidentType='" + incidentType + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    /**
+     * Builder class for Incident.
+     * Implements the Builder Pattern to create Incident instances in a fluent API style.
+     */
+    public static class Builder {
+        private int incidentID;
+        private String incidentType;
+        private String description;
+
+        public Builder incidentID(int incidentID) {
+            this.incidentID = incidentID;
+            return this;
+        }
+
+        public Builder incidentType(String incidentType) {
+            this.incidentType = incidentType;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Incident build() {
+            return new Incident(this);
+        }
+    }
 }
