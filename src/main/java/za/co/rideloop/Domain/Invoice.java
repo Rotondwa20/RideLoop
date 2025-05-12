16d4b4b2f11f0451c43a4dbc6c23395cfee05420
Mzie <mzi.ntutu@gmail.com>
2025-05-12 02:16:57 +0200

    Payment Entity done

---
+++ src/main/java/za/co/rideloop/Domain/Invoice.java
@@ -0,0 +1,161 @@
+package za.co.rideloop.Domain;
+/**
+ * Admin.java
+ * Admin Model Class
+ *
+ * @Author: Mziwamangwevu Ntutu
+ * @Student Number: 217054420
+ **/
+public class Invoice {
+    private int invoiceID;
+    private String invoiceDate;
+    private String dueDate;
+    private String status;
+    private double subtotal;
+    private double taxAmount;
+    private double discountAmount;
+    private double totalAmount;
+    private String paymentMethod;
+    private String paymentReference;
+
+    public Invoice() {
+
+    }
+
+    public Invoice(InvoiceBuilder builder) {
+        this.invoiceID = builder.invoiceID;
+        this.invoiceDate = builder.invoiceDate;
+        this.dueDate = builder.dueDate;
+        this.status = builder.status;
+        this.subtotal = builder.subtotal;
+        this.taxAmount = builder.taxAmount;
+        this.discountAmount = builder.discountAmount;
+        this.totalAmount = builder.totalAmount;
+        this.paymentMethod = builder.paymentMethod;
+        this.paymentReference = builder.paymentReference;
+    }
+
+    public int getInvoiceID() {
+        return invoiceID;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public double getTaxAmount() {
+        return taxAmount;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public String getPaymentReference() {
+        return paymentReference;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "invoiceID=" + invoiceID +
+                ", invoiceDate='" + invoiceDate + '\'' +
+                ", dueDate='" + dueDate + '\'' +
+                ", status='" + status + '\'' +
+                ", subtotal=" + subtotal +
+                ", taxAmount=" + taxAmount +
+                ", discountAmount=" + discountAmount +
+                ", totalAmount=" + totalAmount +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                ", paymentReference='" + paymentReference + '\'' +
+                '}';
+    }
+
+    public static class InvoiceBuilder {
+        private int invoiceID;
+        private String invoiceDate;
+        private String dueDate;
+        private String status;
+        private double subtotal;
+        private double taxAmount;
+        private double discountAmount;
+        private double totalAmount;
+        private String paymentMethod;
+        private String paymentReference;
+
+        public InvoiceBuilder setInvoiceID(int invoiceID) {
+            this.invoiceID = invoiceID;
+            return this;
+        }
+
+        public InvoiceBuilder setInvoiceDate(String invoiceDate) {
+            this.invoiceDate = invoiceDate;
+            return this;
+        }
+
+        public InvoiceBuilder setDueDate(String dueDate) {
+            this.dueDate = dueDate;
+            return this;
+        }
+
+        public InvoiceBuilder setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public InvoiceBuilder setSubtotal(double subtotal) {
+            this.subtotal = subtotal;
+            return this;
+        }
+
+        public InvoiceBuilder setTaxAmount(double taxAmount) {
+            this.taxAmount = taxAmount;
+            return this;
+        }
+
+        public InvoiceBuilder setDiscountAmount(double discountAmount) {
+            this.discountAmount = discountAmount;
+            return this;
+        }
+
+        public InvoiceBuilder setTotalAmount(double totalAmount) {
+            this.totalAmount = totalAmount;
+            return this;
+        }
+
+        public InvoiceBuilder setPaymentMethod(String paymentMethod) {
+            this.paymentMethod = paymentMethod;
+            return this;
+        }
+
+        public InvoiceBuilder setPaymentReference(String paymentReference) {
+            this.paymentReference = paymentReference;
+            return this;
+        }
+
+        public Invoice build() {
+            return new Invoice(this);
+        }
+
+    }
+}
---
+++ src/main/java/za/co/rideloop/Domain/Rental.java
@@ -0,0 +1,163 @@
+package za.co.rideloop.Domain;
+/**
+ * Admin.java
+ * Admin Model Class
+ *
+ * @Author: Mziwamangwevu Ntutu
+ * @Student Number: 217054420
+ **/
+public class Rental {
+    private int rentalID;
+    private int carID;
+    private int customerID;
+    private String startDate;
+    private String endDate;
+    private String pickupLocation;
+    private String dropoffLocation;
+    private int insuranceID;
+
+    private double totalCost;
+    private String status;
+
+    public Rental() {
+    }
+
+    public Rental(RentalBuilder builder) {
+        this.rentalID = builder.rentalID;
+        this.carID = builder.carID;
+        this.customerID = builder.customerID;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+        this.pickupLocation = builder.pickupLocation;
+        this.dropoffLocation = builder.dropoffLocation;
+        this.insuranceID = builder.insuranceID;
+        this.totalCost = builder.totalCost;
+        this.status = builder.status;
+    }
+
+    public int getRentalID() {
+        return rentalID;
+    }
+
+    public int getCarID() {
+        return carID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public String getDropoffLocation() {
+        return dropoffLocation;
+    }
+
+    public int getInsuranceID() {
+        return insuranceID;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "Rental{" +
+                "rentalID=" + rentalID +
+                ", carID=" + carID +
+                ", customerID=" + customerID +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", pickupLocation='" + pickupLocation + '\'' +
+                ", dropoffLocation='" + dropoffLocation + '\'' +
+                ", insuranceID=" + insuranceID +
+                ", totalCost=" + totalCost +
+                ", status='" + status + '\'' +
+                '}';
+    }
+    public static class RentalBuilder {
+        private int rentalID;
+        private int carID;
+        private int customerID;
+        private String startDate;
+        private String endDate;
+        private String pickupLocation;
+        private String dropoffLocation;
+        private int insuranceID;
+        private double totalCost;
+        private String status;
+
+        public RentalBuilder() {
+        }
+
+        public RentalBuilder setRentalID(int rentalID) {
+            this.rentalID = rentalID;
+            return this;
+        }
+
+        public RentalBuilder setCarID(int carID) {
+            this.carID = carID;
+            return this;
+        }
+
+        public RentalBuilder setCustomerID(int customerID) {
+            this.customerID = customerID;
+            return this;
+        }
+
+        public RentalBuilder setStartDate(String startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public RentalBuilder setEndDate(String endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public RentalBuilder setPickupLocation(String pickupLocation) {
+            this.pickupLocation = pickupLocation;
+            return this;
+        }
+
+        public RentalBuilder setDropoffLocation(String dropoffLocation) {
+            this.dropoffLocation = dropoffLocation;
+            return this;
+        }
+
+        public RentalBuilder setInsuranceID(int insuranceID) {
+            this.insuranceID = insuranceID;
+            return this;
+        }
+
+        public RentalBuilder setTotalCost(double totalCost) {
+            this.totalCost = totalCost;
+            return this;
+        }
+
+        public RentalBuilder setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Rental build() {
+            // You can add validation here if needed
+            return new Rental(this);
+        }
+    }
+}
