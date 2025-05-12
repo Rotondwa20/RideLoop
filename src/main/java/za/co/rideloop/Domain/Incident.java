ab5a12f3ecb5b4ca6e95508a9c261e052c23c1f7
RatiaSB <230724477@mycput.ac.za>
2025-05-12 12:02:28 +0200

    added Incident.java

---
+++ src/main/java/za/co/rideloop/Domain/Incident.java
@@ -0,0 +1,15 @@
+package za.co.rideloop.Domain;
+
+/**
+ * RideLoop
+ * Incident.java
+ *
+ * @author : Swatsi Bongani Ratia
+ * @studnr : 230724477
+ * @group : 3I
+ * @date : 5/12/2025
+ * @Java version: "21.0.3" 2024-04-16 LTS
+ */
+public class Incident {
+
+}
