d017cbe63eff736a217728155f19c97a1fc61ae1
RatiaSB <230724477@mycput.ac.za>
2025-05-17 22:04:33 +0200

    created SecurityCompanyFactoryTest and testested with success

---
+++ src/test/java/za/co/rideloop/factory/SecurityCompanyFactoryTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+/**
+*SecurityCompanyFactoryTest.java
+*SecurityCompanyFactoryTest model class
+*@author : Swatsi Bongani Ratia
+*@studnr : 230724477
+*@group  : 3I
+*@Java version: "21.0.3" 2024-04-16 LTS
+*/
+
+class SecurityCompanyFactoryTest {
+  
+}
