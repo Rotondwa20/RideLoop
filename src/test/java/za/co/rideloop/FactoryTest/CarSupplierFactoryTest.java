6cf09b43c421cde95ded6eeb71548bf84b549434
RatiaSB <230724477@mycput.ac.za>
2025-05-19 02:46:56 +0700

    Created CarSupplierFactoryTest. Fixes #56

---
+++ src/test/java/za/co/rideloop/FactoryTest/CarSupplierFactoryTest.java
@@ -0,0 +1,51 @@
+package za.co.rideloop.FactoryTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import za.co.rideloop.Domain.CarSupplier;
+import za.co.rideloop.Factory.CarSupplierFactory;
+
+import java.util.Date;
+/**
+ * CarSupplierFactoryTest.java
+ * CarSupplierFactoryTest model class
+ *
+ * @author : Swatsi Bongani Ratia
+ * @studnr : 230724477
+ * @group : 3I
+ * @Java version: "21.0.3" 2024-04-16 LTS
+ */
+
+class CarSupplierFactoryTest {
+  @Test
+  void testBuildSuccess() {
+      CarSupplier supplier = CarSupplierFactory.build(
+              1,
+              "AutoWorld",
+              "Kruben Naidoo",
+              new Date(),
+              "Active");
+      assertNotNull(supplier);
+      assertEquals("AutoWorld", supplier.getName());
+      assertEquals("Kruben Naidoo", supplier.getContactPerson());
+
+      assertEquals("Active", supplier.getContractStatus());
+  }
+    @Test
+    void testBuildWithEmptyName_shouldThrowException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            CarSupplierFactory.build(
+                    2,
+                    "",
+                    "Swatsi Ratia",
+                    new Date(),
+                    "Pending");
+        });
+        //String expectedMessage = "Invalid Input";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains("Invalid Input"));
+    }
+
+}
