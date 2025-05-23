1389ada801c2409dfeffebd94facd4b76dabee3f
Natasha Njili <natashanjili@gmail.com>
2025-05-23 11:01:50 +0200

    Service package added with LocationService class . Fixes #95

---
+++ src/main/java/za/co/rideloop/Service/LocationService.java
@@ -0,0 +1,36 @@
+package za.co.rideloop.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.co.rideloop.Domain.Location;
+import za.co.rideloop.Repository.LocationRepository;
+
+import java.util.List;
+
+@Service
+public class LocationService {
+    @Autowired
+    private LocationRepository repository;
+
+
+    public Location createLocation(Location location){
+        return this.repository.save(location);
+    }
+
+    public Location readLocation (Integer id){
+        return this.repository.findById(id).orElse(null);
+    }
+
+    public Location updateLocation(Location location){
+        return this.repository.save(location);
+
+    }
+
+    public void delete(Integer id){
+        this.repository.deleteById(id);
+    }
+
+    public List<Location> getAllLocations(){
+        return this.repository.findAll();
+    }
+}
