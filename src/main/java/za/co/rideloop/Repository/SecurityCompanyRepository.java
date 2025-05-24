ccbd8f26538b50a512fc9ff0fa7d18a943c33328
RatiaSB <230724477@mycput.ac.za>
2025-05-24 12:09:39 +0200

    feat(repository): add JPA repository interfaces for CarSupplier, Incident, and SecurityCompany
    Implemented Spring Data JPA repository interfaces for CarSupplier, Incident, and SecurityCompany
    entities to enable standard CRUD operations. These interfaces extend JpaRepository and are
    structured within the appropriate package for separation of concerns.
    
    Fixes #112

---
+++ src/main/java/za/co/rideloop/Repository/SecurityCompanyRepository.java
@@ -0,0 +1,18 @@
+package za.co.rideloop.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.co.rideloop.Domain.SecurityCompany;
+
+/**
+ * RideLoop
+ * SecurityCompanyRepository.java
+ * <p>
+ * author : Swatsi Bongani Ratia
+ * studnr : 230724477
+ * group : 3I
+ * date : 5/24/2025
+ * Java version: "21.0.3" 2024-04-16 LTS
+ */
+public interface SecurityCompanyRepository extends JpaRepository<SecurityCompany, Integer> {
+
+}
